a389e871084519fb47b97d5cca00bacc4b29609a
Amrit Pal Singh <amritpalsinghs369@gmail.com>
2019-04-24 19:08:58 -0700

    menu door effect

--- js/main.js
+++ js/main.js
@@ -1,6 +1,9 @@
 // Select DOM Items
 
 const menuToggler = document.querySelector('.menu-toggler');
+const menu = document.querySelector('.menu');
+const menuNav = document.querySelector('.menu-nav');
+const menuInfo = document.querySelector('.menu-info');
 
 let openMenu = false;
 
@@ -9,10 +12,16 @@ menuToggler.addEventListener('click', toggleMenu);
 function toggleMenu(){
   if (!openMenu){
     menuToggler.classList.add('close');
+    menu.classList.add('open');
+    menuNav.classList.add('open');
+    menuInfo.classList.add('open');
     openMenu = true;
   }
   else{
     menuToggler.classList.remove('close');
+    menu.classList.remove('open');
+    menuNav.classList.remove('open');
+    menuInfo.classList.remove('open');
     openMenu = false;
   }
 }
