66e3ff44fcbf6e25f59b086498568fcd47df59d9
Amrit Pal Singh <amritpalsinghs369@gmail.com>
2019-04-28 16:54:33 -0700

    contact page

--- js/main.js
+++ js/main.js
@@ -39,3 +39,167 @@ function toggleMenu(){
     openMenu = false;
   }
 }
+
+// Contact Page Google map
+
+function initMap(){
+
+  // Map options
+  var options = {
+    zoom: 7,
+    center: {lat: 49.282730, lng: -123.120735},
+    mapTypeControl: false,
+    zoomControl: false,
+    fullscreenControl: false,
+    streetViewControl: false,
+    styles: [
+      {
+        "featureType": "all",
+        "elementType": "labels.text.fill",
+        "stylers": [
+            {
+                "color": "#444444"
+            }
+        ]
+    },
+    {
+        "featureType": "all",
+        "elementType": "labels.icon",
+        "stylers": [
+            {
+              "visibility": "of"
+            }
+        ]
+    },
+    {
+        "featureType": "landscape",
+        "elementType": "all",
+        "stylers": [
+            {
+                "color": "#2fe0b8"
+            }
+        ]
+    },
+    {
+        "featureType": "administrative",
+        "elementType": "geometry",
+        "stylers": [
+            {
+                "color": "#2fe0b8"
+            }
+        ]
+    },
+    {
+        "featureType": "poi",
+        "elementType": "all",
+        "stylers": [
+            {
+                "visibility": "off"
+            }
+        ]
+    },
+    {
+        "featureType": "road",
+        "elementType": "all",
+        "stylers": [
+            {
+                "saturation": -100
+            },
+            {
+                "lightness": 45
+            },
+            {
+              "color": "#4f4f4f"
+            }
+        ]
+    },
+    {
+        "featureType": "road.highway",
+        "elementType": "all",
+        "stylers": [
+            {
+                "visibility": "simplified"
+            },
+            
+        ]
+    },
+    {
+        "featureType": "road.local",
+        "elementType": "all",
+        "stylers": [
+            {
+                "visibility": "off"
+            }
+        ]
+    },
+    {
+        "featureType": "road.arterial",
+        "elementType": "all",
+        "stylers": [
+            {
+                "visibility": "off"
+            }
+        ]
+    },
+    {
+        "featureType": "road",
+        "elementType": "labels",
+        "stylers": [
+            {
+                "visibility": "off"
+            },
+        ]
+    },
+    {
+        "featureType": "road.arterial",
+        "elementType": "labels.icon",
+        "stylers": [
+            {
+                "visibility": "off"
+            }
+        ]
+    },
+    {
+        "featureType": "transit",
+        "elementType": "all",
+        "stylers": [
+            {
+                "visibility": "off"
+            }
+        ]
+    },
+    {
+        "featureType": "water",
+        "elementType": "all",
+        "stylers": [
+            {
+                "color": "#3c3e41"
+            },
+            {
+                "visibility": "on"
+            }
+        ]
+    }
+    ]
+  }
+
+  // New map 
+  var map = new google.maps.Map(document.getElementById('map'), options);
+
+  // Add marder
+  var marker = new google.maps.Marker({
+    position: {lat: 49.178009, lng: -122.854111},
+    map: map,
+    icon: '../img/location2.png'
+  });
+
+  var infoWindow = new google.maps.InfoWindow({
+    content: '<h3 class="home-marker">9651 Salal Place <br> Surrey, BC V3T 5A8  </h3>'
+  });
+
+  marker.addListener('click', function(){
+    infoWindow.open(map, marker);
+  });
+
+  infoWindow.open(map, marker);
+}
